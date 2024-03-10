6e9e5aa7a22fdbfa2fc73bb55539c2db978485ed
xwhoknowz <jeremiah2498@gmail.com>
2024-03-10 16:09:14 -0500

    added auth to utils

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,99 +1,99 @@
-// const { game, player, squad } = require("../models/index");
+const { Game, Player, Squad } = require("../models/index");
 
-// const resolvers = {
-//   Query: {
-//     player: async (parent, { username }) => {
-//       return player.findOne({ username }).populate("squads", "friends");
-//     },
-//     games: async () => {
-//       return game.find();
-//     },
-//     games: async (parent, { gameId }) => {
-//       return game.findOne({ _id: gameId }).populate("squads");
-//     },
-//     squad: async (parent, { squadId }) => {
-//       return squad.findOne({ _id: squadId });
-//     },
-//     squads: async (parent, { username }) => {
-//       return squad.find({ username });
-//     },
-//     squads: async (parent, { gameName }) => {
-//       return squad.find({ name: gameName });
-//     },
-//   },
+const resolvers = {
+  Query: {
+    player: async (parent, { username }) => {
+      return Player.findOne({ username }).populate("squads", "friends");
+    },
+    games: async () => {
+      return Game.find();
+    },
+    games: async (parent, { gameId }) => {
+      return Game.findOne({ _id: gameId }).populate("squads");
+    },
+    squad: async (parent, { squadId }) => {
+      return Squad.findOne({ _id: squadId });
+    },
+    squads: async (parent, { username }) => {
+      return Squad.find({ username });
+    },
+    squads: async (parent, { gameName }) => {
+      return Squad.find({ name: gameName });
+    },
+  },
 
-//   Mutation: {
-//     addPlayer: async (parent, { username, email, password }) => {
-//       const player = await player.create({ username, email, password });
-//       const token = signToken(player);
-//       return { token, player };
-//     },
-//     login: async (parent, { email, password }) => {
-//       const player = await player.findOne({ email });
-//       if (!player) {
-//         throw AuthenticationError;
-//       }
-//       pwMatch = await player.isCorrectPassword(password);
-//       if (!pwMatch) {
-//         throw AuthenticationError;
-//       }
-//       const token = signToken(player);
-//       return { token, player };
-//     },
-//     removePlayer: async (parent, {playerId}) => {
-//         return player.findOneAndDelete({_id: playerId})
-//     },
-//     addFriend: async (parent, {playerId, username}) => {
-//         return player.findOneAndUpdate(
-//             {_id: playerId},
-//             {$addToSet: {friends: username}},
-//             {new: true, runValidators: true}
-//         )
-//     },
-//     removeFriend: async (parent, {playerId}) => {
-//        return player.findOneAndUpdate(
-//         {_id: playerId},
-//         {$pull: {friends: username}},
-//         {new: true, runValidators: true}
-//        )
-//     },
-//     addGame: async (parent, {name, image, platforms, rating, review}) => {
-//         return game.create({name, image, platforms, rating, review})
-//     },
-//     addSquad: async (squadName, playerCount, ranked, playStyle) => {
-//         const squad = await squad.create({
-//             squadName, playerCount, ranked, playStyle
-//         })
+  Mutation: {
+    addPlayer: async (parent, { username, email, password }) => {
+      const player = await Player.create({ username, email, password });
+      const token = signToken(player);
+      return { token, player };
+    },
+    login: async (parent, { email, password }) => {
+      const player = await Player.findOne({ email });
+      if (!player) {
+        throw AuthenticationError;
+      }
+      pwMatch = await player.isCorrectPassword(password);
+      if (!pwMatch) {
+        throw AuthenticationError;
+      }
+      const token = signToken(player);
+      return { token, player };
+    },
+    removePlayer: async (parent, {playerId}) => {
+        return Player.findOneAndDelete({_id: playerId})
+    },
+    addFriend: async (parent, {playerId, username}) => {
+        return Player.findOneAndUpdate(
+            {_id: playerId},
+            {$addToSet: {friends: username}},
+            {new: true, runValidators: true}
+        )
+    },
+    removeFriend: async (parent, {playerId}) => {
+       return Player.findOneAndUpdate(
+        {_id: playerId},
+        {$pull: {friends: username}},
+        {new: true, runValidators: true}
+       )
+    },
+    addGame: async (parent, {name, image, platforms, rating, review}) => {
+        return Game.create({name, image, platforms, rating, review})
+    },
+    addSquad: async (squadName, playerCount, ranked, playStyle) => {
+        const squad = await Squad.create({
+            squadName, playerCount, ranked, playStyle
+        })
 
-//         await player.findOneAndUpdate(
-//             {_id: playerId},
-//             {$addToSet: {squads: squad}},
-//         )
+        await Player.findOneAndUpdate(
+            {_id: playerId},
+            {$addToSet: {squads: squad}},
+        )
 
-//         await game.findOneAndUpdate(
-//             {_id: gameId},
-//             {$addToSet: {squads: squad}},
-//         )
-//         return squad
-//     },
-//     removeSquad: async (parent, {squadId}) => {
-//         const squad = await squad.findOneAndDelete({
-//             _id: squadId
-//         })
+        await Game.findOneAndUpdate(
+            {_id: gameId},
+            {$addToSet: {squads: squad}},
+        )
+        return squad
+    },
+    removeSquad: async (parent, {squadId}) => {
+        const squad = await Squad.findOneAndDelete({
+            _id: squadId
+        })
 
-//         await player.findOneAndUpdate(
-//             {_id: playerId},
-//             {$pull: {squads: squad}},
-//         )
+        await Player.findOneAndUpdate(
+            {_id: playerId},
+            {$pull: {squads: squad}},
+        )
 
-//         await game.findOneAndUpdate(
-//             {_id: gameId},
-//             {$pull: {squads: squad}},
-//         )
-//         return squad;
-//     },
-//   },
-// };
+        await Game.findOneAndUpdate(
+            {_id: gameId},
+            {$pull: {squads: squad}},
+        )
+        return squad;
+    },
+  },
+};
 
 
-// module.exports = resolvers;
+module.exports = resolvers;
